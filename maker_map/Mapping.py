487f91e5a90acce1421d2329477b124754c78a53
Teju Reddy <tejaswini_muthumula@student.uml.edu>
2022-01-06 17:37:49 -0500

    Mapping the volcanoes_population

--- maker_map/Mapping.py
+++ maker_map/Mapping.py
@@ -1,21 +1,34 @@
 import folium
+from folium.map import LayerControl
 import pandas
 
 data = pandas.read_csv("Volcanoes.txt")
-#df = pandas.DataFrame(data)
-#print(df)
-#df1= df.iloc[:,-2:]
-#print(df1)
 
-#print(data)
+
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
-map= folium.Map(location= [36.8,-99.09],zoom_start=6, title ="Mapbox Bright")
+def color_producer(elevation):
+    if elevation<1000:
+        return"green"
+    elif 1000< elevation < 3000:
+        return "red"
+    else:
+        return "pink"
+map= folium.Map(location= [36.8,-99.09],zoom_start=6, title ="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My map")
+fgv = folium.FeatureGroup(name="Volacanoes")
 
-for ln,lo in zip(lat,lon):
-    fg.add_child(folium.Marker(location=[ln,lo], popup="Hi I'm a marker", icon= folium.Icon(color ="red")))
-map.add_child(fg)
+for ln,lo,el in zip(lat,lon,elev):
+    fgv.add_child(folium.Marker(location=[ln,lo], popup=str(el), fill_color =color_producer(el),color = "grey",fill_opacity =0.75))
+
+fgp = folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(), 
+style_function=lambda x: {'fillColor' : "green" if x['properties']['POP2005']< 10000000 
+else "orange" if 1000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
